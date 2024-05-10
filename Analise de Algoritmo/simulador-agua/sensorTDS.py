a9c7cf44021d99234b807ac0f96300c90a648cfd
JGbarreto <joao.conceicao@sptech.school>
2024-05-09 22:16:17 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Analise de Algoritmo/simulador-agua/sensorTDS.py
+++ Analise de Algoritmo/simulador-agua/sensorTDS.py
@@ -2,16 +2,20 @@ import time
 import random
 import mysql.connector
 import datetime
+from azure.iot.device import IoTHubDeviceClient, Message
 
-config = {
-    'user': 'root',
-    'password': 'toor',
-    'host': 'localhost',
-    'database': 'sensores',
-    'port': '3306',
-}
-connection = mysql.connector.connect(**config)
-cursor = connection.cursor()
+# config = {
+#     'user': 'root',
+#     'password': 'toor',
+#     'host': 'localhost',
+#     'database': 'sensores',
+#     'port': '3306',
+# }
+conn_str = "HostName=hub-g2.azure-devices.net;DeviceId=tds-sensor;SharedAccessKey=ZMrvYHdAIyPJ0W2ghOKXaZMH0GoeetMwPAIoTP7qSb4="
+client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+
+# connection = mysql.connector.connect(**config)
+# cursor = connection.cursor()
 numero_ant = 50
 contador = 0
 dia = 1
@@ -32,12 +36,12 @@ while(True):
             numero=random.randint(200,600)
             state='segura'
     numero_ant=numero    
-    cursor.execute(f'INSERT INTO tds_sensor VALUES (null, "{ano}-{mes_str if mes<10 else mes}-{dia_str if dia<10 else dia}",{numero})')
-    connection.commit()
-    rows = cursor.fetchall()
-    
+    # cursor.execute(f'INSERT INTO tds_sensor VALUES (null, "{ano}-{mes_str if mes<10 else mes}-{dia_str if dia<10 else dia}",{numero})')
+    # connection.commit()
+    # rows = cursor.fetchall()
+    client.send_message(numero)
     print(f'a água está com o valor de TDS de {numero}mg/L, {state} para o consumo')
-    time.sleep(5)
+    
     contador +=1
     dia+=1
     if dia >30:
@@ -48,3 +52,4 @@ while(True):
         ano+=1
     dia_str = f'0{dia}'
     mes_str = f'0{mes}'
+    time.sleep(5)
